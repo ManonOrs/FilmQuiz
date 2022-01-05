77df1e32d034f147dcce5c9f73a07a1ae111176b
Manon <manon.orsini5@gmail.com>
2022-01-05 17:42:41 +0100

    Initial commit

---
+++ app/src/main/java/com/hb/filmquiz/MainActivity.java
@@ -0,0 +1,112 @@
+package com.hb.filmquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hb.filmquiz.pojos.Question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvQuestion;
+    private TextView tvScore;
+    private Button btnTrue;
+    private Button btnFalse;
+    private Button btnRestart;
+    private List<Question> questions = new ArrayList<Question>();
+    private int indexQuestion = 0;
+    private int score = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // on récupère les éléments du layout
+        tvQuestion = findViewById(R.id.tvQuestion);
+        tvScore = findViewById(R.id.tvScore);
+        btnTrue = findViewById(R.id.btnTrue);
+        btnFalse = findViewById(R.id.btnFalse);
+        btnRestart = findViewById(R.id.btnRestart);
+
+        // ajoute les questions a la liste
+        questions.add(new Question(getString(R.string.question_ai), true));
+        questions.add(new Question(getString(R.string.question_taxi_driver), true));
+        questions.add(new Question(getString(R.string.question_2001), false));
+        questions.add(new Question(getString(R.string.question_reservoir_dogs), true));
+        questions.add(new Question(getString(R.string.question_citizen_kane), false));
+
+        tvQuestion.setText(questions.get(indexQuestion).getText());
+        tvScore.setText(getString(R.string.score).concat(": " + score));
+
+        btnRestart.setVisibility(View.GONE);
+
+        btnTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (questions.get(indexQuestion).isAnswer()) {
+                    score = score + 1;
+                    Toast toast = Toast.makeText(getApplicationContext(), "VRAI", Toast.LENGTH_LONG);
+                    toast.show();
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "FAUX", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+                indexQuestion = indexQuestion + 1;
+                tvScore.setText(getString(R.string.score).concat(": " + score));
+                if (indexQuestion < questions.size()) {
+                    tvQuestion.setText(questions.get(indexQuestion).getText());
+                } else {
+                    btnRestart.setVisibility(View.VISIBLE);
+                    btnFalse.setVisibility(View.GONE);
+                    btnTrue.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        btnFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (questions.get(indexQuestion).isAnswer()) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "FAUX", Toast.LENGTH_LONG);
+                    toast.show();
+                } else {
+                    score = score + 1;
+                    Toast toast = Toast.makeText(getApplicationContext(), "VRAI", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+                indexQuestion = indexQuestion + 1;
+                tvScore.setText(getString(R.string.score).concat(": " + score));
+
+                if (indexQuestion < questions.size()) {
+                    tvQuestion.setText(questions.get(indexQuestion).getText());
+
+                } else {
+                    btnRestart.setVisibility(View.VISIBLE);
+                    btnFalse.setVisibility(View.GONE);
+                    btnTrue.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        btnRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                score = 0;
+                indexQuestion = 0;
+                tvQuestion.setText(questions.get(indexQuestion).getText());
+                tvScore.setText(getString(R.string.score).concat(": " + score));
+                btnRestart.setVisibility(View.GONE);
+                btnFalse.setVisibility(View.VISIBLE);
+                btnTrue.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/hb/filmquiz/pojos/Question.java
@@ -0,0 +1,42 @@
+package com.hb.filmquiz.pojos;
+
+public class Question {
+
+    private int id;
+    private String text;
+    private boolean answer;
+
+    public Question(String text, boolean answer){
+        this.text = text;
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(boolean answer) {
+        this.answer = answer;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", answer=" + answer +
+                '}';
+    }
+}
